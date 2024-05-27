f378663fb109a2ce7a2e929bf81d2333fc7de20d
FatimaAlexandra <falexandra@outlook.es>
2024-05-27 01:40:26 -0600

    Funcionalidad agregar Actividad Turistica

---
+++ app/src/main/java/com/example/pdmlugaresturisticos/models/DestinoTuristico.kt
@@ -0,0 +1,8 @@
+package com.example.pdmlugaresturisticos.models
+
+data class DestinoTuristico(
+    val idDestinoTuristico: Int = 0,
+    val nombre: String,
+    val descripcion: String,
+    val imagen: String
+)
