b41b1c4d2beb2de3f74a3a4d7873f87f27055a9f
FatimaAlexandra <falexandra@outlook.es>
2024-06-04 20:13:04 -0600

    contacts

---
+++ app/src/main/java/com/example/pdmlugaresturisticos/activity_contacts.kt
@@ -0,0 +1,11 @@
+package com.example.pdmlugaresturisticos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_contacts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts)
+    }
+}
